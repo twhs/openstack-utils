81841b6eda7292639be4bf5abf44a56c63230297
twhs <twhs768@gmail.com>
2015-09-01 00:48:12 +0900

    [add] endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -8,7 +8,12 @@ from keystoneclient.auth.identity import v2
 from keystoneclient import session
 from keystoneclient.v2_0 import client
 
-base = 'https://helion-horizon.cu-mo.jp'
+DRY_RUN=True
+
+protocol = 'http'
+public = '192.168.11.100'  
+internal = '10.0.0.1'
+admin = '10.0.0.1'
 
 user = os.getenv('OS_USERNAME')
 password = os.getenv('OS_PASSWORD')
@@ -23,15 +28,23 @@ if user is None or password is None or tenant is None or region is None or auth_
 #              publicURL, internalURL, adminURL の順
 # サービス名: [(ポート, パス), ...]
 new_endpoints = {
-    "keystone": [(5000, "v2.0")],
-    "nova": [],
-    "neutron": [],
+    "keystone": [(5000, "v2.0"), (5000, "v2.0"), (35357, "v2.0")],
+    "ceilometer": [(8777, ""),(8777, ""),(8777, "")],
+    "cinder": [(8776, "v1/%(tenant_id)s"),(8776, "v1/%(tenant_id)s"),(8776, "v1/%(tenant_id)s")],
+    "cinderv2": [(8776, "v2/%(tenant_id)s"),(8776, "v2/%(tenant_id)s"),(8776, "v2/%(tenant_id)s")],
+    "glance": [(9292, ""),(9292, ""),(9292, "")],
+    "neutron": [(9696, ""),(9696, ""),(9696, "")],
+    "nova": [(8774, "v2/%(tenant_id)s"),(8774, "v2/%(tenant_id)s"),(8774, "v2/%(tenant_id)s")],
+    "nova_ec2": [(8773, "services/Cloud"),(8773, "services/Cloud"),(8773, "services/Admin")],
+    "novav3": [(8774, "v3"),(8774, "v3"),(8774, "v3")],
+    "swift": [(8080, "v1/AUTH_%(tenant_id)s"),(8080, "v1/AUTH_$(tenant_id)s"),(8080, "")],
+    "swift_s3": [(8080, ""),(8080, ""),(8080, "")]
 }
 
 auth = v2.Password(auth_url=auth_url, username=user, password=password, tenant_name=tenant)
 sess = session.Session(auth=auth, verify=False)
 
-cli = client.Client(session=ss)
+cli = client.Client(session=sess, region_name=region)
 
 services = cli.services.list()
 
@@ -39,20 +52,23 @@ services = cli.services.list()
 old_endpoints = cli.endpoints.list()
 
 # サービスごとに endpoint を変更
+url_template = protocol + "://%s:%d/%s"
 for service in services:
     endpoint = new_endpoints[service.name]
-    publicURL = "%s:%d/%s" % (base, endpoint[0][0], endpoint[0][1])
-    internalURL = "%s:%d/%s" % (base, endpoint[1][0], endpoint[1][1])
-    adminURL = "%s:%d/%s" % (base, endpoint[2][0], endpoint[2][1])
-    cli.endpoints.create(
-        region=region,
-        service_id=service.id,
-        publicURL=publicURL,
-        internalURL=internalURL,
-        adminURL=adminURL
-    )
+    publicURL = url_template % (public, endpoint[0][0], endpoint[0][1])
+    internalURL = url_template % (internal, endpoint[1][0], endpoint[1][1])
+    adminURL = url_template % (admin, endpoint[2][0], endpoint[2][1])
+    if not DRY_RUN:
+        cli.endpoints.create(
+            region=region,
+            service_id=service.id,
+            publicURL=publicURL,
+            internalURL=internalURL,
+            adminURL=adminURL
+        )
+    print("[%s] public=%s internal=%s admin=%s" % (service.name, publicURL, internalURL, adminURL))
 
 # 変更前の endpoint を削除
 for endpoint in old_endpoints:
-    print(endpoint)
-    cli.endpoints.delete(endpoint.id)
+    if not DRY_RUN:
+        cli.endpoints.delete(endpoint.id)
